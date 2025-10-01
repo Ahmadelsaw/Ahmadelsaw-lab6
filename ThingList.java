d51db39c8c1808ec2855973da05a2d0472041c5d
Ahmadelsaw <ahmade@gwmail.gwu.edu>
2025-10-01 14:25:16 -0400

    type C and thinglist

--- ThingList.java
+++ ThingList.java
@@ -1,3 +1,43 @@
+import java.io.PrintStream;
+import java.util.Random;
+
 public class ThingList {
-    
+
+  // simple singly-linked list
+  private static class Node {
+    Thing val;
+    Node next;
+    Node(Thing v) {this.val=v;}
+  }
+
+  private Node head;
+  private Node tail;
+
+  public void add(Thing t) {
+    Node n=new Node(t);
+    if (head==null) {
+      head=tail = n;
+    } else {
+      tail.next = n;
+      tail=n;
+    }
+  }
+
+  // advance one round for every Thing:
+  // 1) let each decide turning (polymorphic)
+  // 2) step forward
+  public void moveAll(Random rand) {
+    for (Node cur = head; cur != null; cur = cur.next) {
+      cur.val.maybeTurn(rand);
+      cur.val.step();
+    }
+  }
+
+  // print current round for the plotter
+  public void printAll(PrintStream out) {
+    for (Node cur = head; cur != null; cur = cur.next) {
+      out.println(cur.val.toString());
+    }
+    out.println("done");
+  }
 }
--- TypeC.java
+++ TypeC.java
@@ -1,3 +1,17 @@
-public class TypeC {
+import java.util.Random;
+ public class TypeC extends Thing {
+    private boolean turnright = true; // toggle direction each time
     
-}
+    // Constructor
+    public TypeC(int r, int c, int d, char l) {
+        super(r, c, d, l);
+    }
+
+    @Override
+    public void maybeTurn(Random rand) {
+        if (turnright) rightTurn();
+        else leftTurn();
+        turnright = !turnright;
+
+        }
+    }
