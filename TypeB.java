38bda7e3d2474920019b362afffb2397bc5bd49d
Ahmadelsaw <ahmade@gwmail.gwu.edu>
2025-10-01 11:23:07 -0400

    Type B addition

--- TypeB.java
+++ TypeB.java
@@ -1,3 +1,20 @@
-public class TypeB {
+import java.util.Random;
+
+public class TypeB extends Thing {
+    private int rounds = 0;
     
+    // Constructor
+    public TypeB(int r, int c, int d, char l) {
+        super(r, c, d, l);
+    }
+
+    @Override
+    public void maybeTurn(Random rand) {
+        rounds++;
+        if(rounds % 10 == 0){
+        int i = rand.nextInt(3);
+        if (i == 0) rightTurn();
+        else if (i == 1) leftTurn();
+        }
+    }
 }
