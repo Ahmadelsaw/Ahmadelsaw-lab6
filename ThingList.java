e2760824bc4482c5b0e8c05625bcd329e3d69b1e
Ahmadelsaw <ahmade@gwmail.gwu.edu>
2025-10-03 15:26:26 -0400

    thinglist changes

--- ThingList.java
+++ ThingList.java
@@ -4,16 +4,16 @@ import java.util.Random;
 public class ThingList {
 
   // simple singly-linked list
-  private static class Node {
-    Thing val;
+  private class Node {
+    Thing data;
     Node next;
-    Node(Thing v) {this.val=v;}
+    Node(Thing v) {this.data =v;}
   }
 
   private Node head;
   private Node tail;
 
-  public void add(Thing t) {
+  public void addThing(Thing t) {
     Node n=new Node(t);
     if (head==null) {
       head=tail = n;
@@ -28,16 +28,17 @@ public class ThingList {
   // 2) step forward
   public void moveAll(Random rand) {
     for (Node cur = head; cur != null; cur = cur.next) {
-      cur.val.maybeTurn(rand);
-      cur.val.step();
+      cur.data.maybeTurn(rand);
+      cur.data.step();
     }
   }
 
   // print current round for the plotter
-  public void printAll(PrintStream out) {
+  public void printAll() {
     for (Node cur = head; cur != null; cur = cur.next) {
-      out.println(cur.val.toString());
+      System.out.println(cur.data.toString());
     }
-    out.println("done");
+    System.out.println("done");
+    System.out.flush();
   }
 }
